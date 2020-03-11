c7d20940f7ad4657d87be065b89dbf27f2499cdd
Tunccc <tuncerman78@gmail.com>
2020-03-11 21:19:40 +0300

    bcrypt

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/User')
 /* GET home page. */
@@ -10,17 +11,21 @@ router.get('/', function(req, res, next) {
 router.post('/register', function(req, res, next) {
   const { username, password} = req.body;
 
-  const user = new User({
-    username,
-    password
-  });
+  bcrypt.hash(password, 10).then((hash)=> {
+    const user = new User({
+      username,
+      password:hash
+    });
+  
+    const promise = user.save();
+    promise.then((data)=>{
+      res.json(data)
+    }).catch((err)=>{
+      res.json(err)
+    })
+});
 
-  const promise = user.save();
-  promise.then((data)=>{
-    res.json(data)
-  }).catch((err)=>{
-    res.json(err)
-  })
+ 
 });
 
 module.exports = router;
